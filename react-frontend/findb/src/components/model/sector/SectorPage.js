2589d2910e87366f744b7e7c9f77aa3337d6a6ff
Mabel Egbirika <maybelle@Mabels-MacBook-Pro.local>
2024-07-14 14:26:23 -0500

    Added embedded videos for sector

--- react-frontend/findb/src/components/model/sector/SectorPage.js
+++ react-frontend/findb/src/components/model/sector/SectorPage.js
@@ -18,16 +18,44 @@ function SectorPage() {
         );
     });
 
+    let video;
+    //Display video for sector
+    if (sectorKey === "technology") {
+        video = <iframe width="560" height="315" src="https://www.youtube.com/embed/6koYf5GmZ48?si=V_rgvk5IV_vLFvKz" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    } else if (sectorKey === "financial-services") {
+        video = <iframe width="560" height="315" src="https://www.youtube.com/embed/xXStP80NwuM?si=UYZ_zvyCK56tRmue" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    } else if (sectorKey === "healthcare") {
+        video = <iframe width="560" height="315" src="https://www.youtube.com/embed/nkdvvrld5tQ?si=iBX83fBlSFJVqMGH" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    } else if (sectorKey === "consumer-cyclical") {
+        video = <iframe width="560" height="315" src="https://www.youtube.com/embed/MdhUJWoA-Bc?si=YqiTY9MkGjlpmbdR" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    } else if (sectorKey === "communication-services") {
+        video = <iframe width="560" height="315" src="https://www.youtube.com/embed/WIeKQ7hh_XQ?si=rIgnDe5TMJDU66Fp" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    } else if (sectorKey === "industrials") {
+        video = <iframe width="560" height="315" src="https://www.youtube.com/embed/meRKd2a9p9w?si=qEvynAlG9nPdubC_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    } else if (sectorKey === "consumer-defensive") {
+        video = <iframe width="560" height="315" src="https://www.youtube.com/embed/ZnmpPIY4vkI?si=DKm_CG7LlajGXODL" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    } else if(sectorKey === "energy") {
+        video = <iframe width="560" height="315" src="https://www.youtube.com/embed/bBcOQWosT1w?si=2We34F2t-60cjdRo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    } else if (sectorKey === "basic-materials") {
+        video = <iframe width="560" height="315" src="https://www.youtube.com/embed/uPCe2jzS5nA?si=zfY9Q2naOR9o47w0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    } else if (sectorKey === "real-estate") {
+        video = <iframe width="560" height="315" src="https://www.youtube.com/embed/WlqYhER9Dd8?si=4ZDkZoCaB8QeRd-p" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    } else if(sectorKey === "utilities") {
+        video = <iframe width="560" height="315" src="https://www.youtube.com/embed/1s375WFZAng?si=yr6tLz9DxRP7zoYm" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    } else {
+        video = <iframe width="560" height="315" src="" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    }
+
     //Display information for instance
     if (data !== undefined) {
         let list_items = []
         for (let a in data) {
-            if(a === "top_index") {
+            if (a === "top_index") {
                 list_items.push(<ListGroup.Item><strong>{a}: </strong> <Link to={`/indexes/${data[a]}`}> {data[a]} </Link></ListGroup.Item>)
-            } else if(a === "top_stock") {
+            } else if (a === "top_stock") {
                 list_items.push(<ListGroup.Item><strong>{a}: </strong> <Link to={`/stocks/${data[a]}`}> {data[a]} </Link></ListGroup.Item>)
             } else {
-                if(a !== "name" || a !== "sector_key") {
+                if (a !== "name" || a !== "sector_key") {
                     list_items.push(<ListGroup.Item><strong>{a}: </strong>{data[a]}</ListGroup.Item>)
                 }
             }
@@ -35,9 +63,14 @@ function SectorPage() {
         output.push(
             <Container>
                 <h1>{data.name}</h1>
-                <ListGroup>
-                   {list_items}
-                </ListGroup>
+                <div className='pt-3'>
+                    {video}
+                </div>
+                <div className='pt-3'>
+                    <ListGroup>
+                        {list_items}
+                    </ListGroup>
+                </div>
             </Container>
         )
 
